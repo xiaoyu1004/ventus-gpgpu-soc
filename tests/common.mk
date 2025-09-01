b37d57c9ce94af1d9c940f91dc61ba44cefddb1c
hongzhang.yu <15216142871@163.com>
2025-09-01 18:04:52 +0800

    compile to asm and llvm ir

--- tests/common.mk
+++ tests/common.mk
@@ -1,16 +1,16 @@
-ROOT_DIR := $(realpath ../../)
 RTL_SIM_DIR := $(ROOT_DIR)/rtlsim
 RUNTIME_DIR := $(ROOT_DIR)/runtime
 
 STARTUP_ADDR ?= 0x80000000
 
-LLVM_VENTUS := /opt/workspace/ventus/llvm-project/install
+LLVM_VENTUS := /home/xiaoyu/ventus_home/llvm-project/install
 
 CC  = $(LLVM_VENTUS)/bin/clang 
 CXX = $(LLVM_VENTUS)/bin/clang++ 
 AR  = $(LLVM_VENTUS)/bin/llvm-ar
 DP  = $(LLVM_VENTUS)/bin/llvm-objdump
 CP  = $(LLVM_VENTUS)/bin/llvm-objcopy
+llc = $(LLVM_VENTUS)/bin/llc
 
 CFLAGS += -O3 -cl-std=CL2.0 -target riscv32 -mcpu=ventus-gpgpu -nodefaultlibs
 CFLAGS += -I$(RUNTIME_DIR) -I$(LLVM_VENTUS)/include -I$(LLVM_VENTUS)/libclc/generic/include
@@ -20,20 +20,26 @@ LIBC_LIB += -L$(LLVM_VENTUS)/lib $(LLVM_VENTUS)/lib/crt0.o -lworkitem
 
 LDFLAGS += -Wl,-T,$(LLVM_VENTUS)/../utils/ldscripts/ventus/elf32lriscv.ld $(LIBC_LIB)
 
-kernel: $(PROJECT).elf $(PROJECT).bin $(PROJECT).dump.s
+kernel: $(PROJECT).elf $(PROJECT).ll $(PROJECT).s $(PROJECT).bin $(PROJECT).dump.s
 all: $(PROJECT)
 
 $(PROJECT).dump.s: $(PROJECT).elf
-	$(DP) -D $< > $@
+	$(DP) -D --mattr=+v,+zfinx $< > $@
 
 $(PROJECT).bin: $(PROJECT).elf
 	$(CP) -O binary $< $@
 
-$(PROJECT).elf: $(SRCS)
+$(PROJECT).elf: $(KNL_SRCS)
 	$(CC) $(CFLAGS) $^ $(LDFLAGS) -o $@
 
-.depend: $(SRCS)
+$(PROJECT).ll: $(KNL_SRCS)
+	$(CC) -O3 -S -cl-std=CL2.0 -target riscv32 -mcpu=ventus-gpgpu -emit-llvm $^ -o $@
+
+$(PROJECT).s: $(PROJECT).ll
+	$(llc) -O3 -mtriple=riscv32 -mcpu=ventus-gpgpu $^ -o $@
+
+.depend: $(KNL_SRCS)
 	$(CC) $(CFLAGS) -MM $^ > .depend;
 
 clean:
-	rm -rf *.elf *.bin *.dump.s *.log .depend
+	rm -rf *.elf *.bin *.dump.s *.log .depend $(PROJECT) *.ll *.s
--- tests/vecadd/vecadd.cl
+++ tests/vecadd/vecadd.cl
@@ -1,6 +1,19 @@
-__kernel void vecadd(__global const float *src, __global const float *dst, int size) {
+__kernel void vecadd(__global const float *src, __global float *dst, float a,
+                     float b, int size) {
   int tid = get_global_id(0);
+  // if (tid < size) {
+  //   dst[tid] = a * src[tid];
+  // } else {
+  //   if (tid % 2 == 0) {
+  //     dst[tid] = b * src[tid];
+  //   } else {
+  //     dst[tid] = (b + a) * src[tid];
+  //   }
+  // }
+
   if (tid < size) {
-    dst[tid] = 4 * src[tid]; 
+    dst[tid] = 4 * src[tid];
+  } else {
+    dst[tid] = 8 * src[tid];
   }
 }
